6e33ddf847f362086e681ddf2f9dedff3d759630
seungsoo <seung_soos@naver.com>
2023-08-21 22:55:09 +0900

    [Section5] - Bean  Validation 추가

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,21 +2,24 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원이상")
 public class Item {
+    @NotNull(groups = UpdateCheck.class)
     private Long id;
-    @NotBlank
+    @NotBlank(groups = {UpdateCheck.class, SaveCheck.class})
     private String itemName;
-    @NotNull
-    @Range(min = 1000, max = 1000000)
+    @NotNull(groups = {UpdateCheck.class, SaveCheck.class})
+    @Range(min = 1000, max = 1000000, groups = {UpdateCheck.class, SaveCheck.class})
     private Integer price;
-    @NotNull
-    @Max(9999)
+    @NotNull(groups = {UpdateCheck.class, SaveCheck.class})
+    @Max(value = 9999, groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
